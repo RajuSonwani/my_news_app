95a2ce553df9d8ca4dc522820352caddf54ccb23
Raju Kumar Sonwani <63111674+RajuSonwani@users.noreply.github.com>
2021-03-21 15:43:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ const app = express();
 const axios = require("axios");
 const port = process.env.PORT||8000;
 
-//Static Files;
-// app.use(express.static("public"))
-// // app.use("/css",express.static(__dirname+"public/css"))
-// // app.use("/img",express.static(__dirname+"public/img"))
-// // app.use("/js",express.static(__dirname+"public/js"))
+Static Files;
+app.use(express.static("public"))
+// app.use("/css",express.static(__dirname+"public/css"))
+// app.use("/img",express.static(__dirname+"public/img"))
+// app.use("/js",express.static(__dirname+"public/js"))
 
-// //Templating Engine;
-// // app.set("views",path.join(__dirname,"views"));
-// app.set("views", __dirname+"/src/views");
-// app.set("view engine", "ejs");
+//Templating Engine;
+// app.set("views",path.join(__dirname,"views"));
+app.set("views", __dirname+"/src/views");
+app.set("view engine", "ejs");
 
-// //express
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}))
+//express
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
-// //Routes
-// const newsRouter = require("./src/routes/news");
-// app.use(newsRouter);
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
-});
+//Routes
+const newsRouter = require("./src/routes/news");
+app.use(newsRouter);
+
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello World!");
+// });
 
 // Listning on Port 8000;
 app.listen(port,()=>{
